f8ee0d220c08848a7c1bb525570b47f878eb6a2d
Your Name <you@example.com>
2023-02-25 14:24:47 +0300

    Добавьте файлы проекта.

---
+++ LabsTasks/Practica1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsTasks
+{
+    internal class Practica1
+    {
+
+        public static void Search(int[] arr, int searchValue)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == searchValue)
+                    Console.WriteLine(arr[i]);
+            }
+        }
+
+        public static void SearchWithBarrier(int[] arr, int searchValue)
+        {
+            var arrCopy = new int[arr.Length + 1];
+
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arrCopy[i] = arr[i];
+            }
+
+            //[1,2,3]  Length - 3, indexMax - 2
+            //arrCopy Length - 6, indexMax - 5
+            arrCopy[arrCopy.Length - 1] = searchValue;
+
+            Search(arrCopy, searchValue);  
+        }
+    }
+}
---
+++ LabsTasks/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LabsTasks;
+
+var arr = new int[] { 1, 3, 5, 8, 9 };
+var element = 5;
+var elemntWithBarrier = 11;
