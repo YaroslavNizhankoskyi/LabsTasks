8a9673b1ab3582f0cf5f99f770b459e9b22be19a
yarosfaros <yaroslav.nizhankovskyi@gmail.com>
2023-03-16 19:37:01 +0200

    lab2_9

---
+++ LabsTasks/Lab2_9.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsTasks
+{
+    internal static class Lab2_9
+    {
+        public static void Calculate(string str = "1+3*4/11")
+        {
+            str = RemoveGarbageSymbols(str);
+
+            var symbols = new char[] { '*', '+', '-', '/' };
+
+            var calcList = str.ToList();
+
+            var operations = calcList
+                .FindAll(x => symbols.Contains(x))
+                .ToList(); 
+
+            for(var i = 0; i < calcList.Count; i++)
+            {
+                if (symbols.Contains(calcList[i]))
+                {
+                    calcList[i] = ',';
+                }
+            }
+
+            var numsString = new string(calcList.ToArray());
+            var nums = numsString
+                .Split(',')
+                .Select(x => int.Parse(x))  
+                .ToList();
+
+            var result = int.MinValue;
+
+            if(nums.Count - 1 == operations.Count)
+            {
+                while (nums.Count > 0)
+                {
+                    if(result == int.MinValue)
+                    {
+                        var leftOp = nums[0];
+                        var rightOp = nums[1];
+                        var operation = operations[0];
+
+                        //switch
+
+                        result = CalcOperation(leftOp, rightOp, operation);
+                        operations.RemoveAt(0);
+                        nums.RemoveAt(0);
+                        nums.RemoveAt(0);
+                    }else
+                    {
+                        result = CalcOperation(result, nums[0], operations[0]);
+                        operations.RemoveAt(0);
+                        nums.RemoveAt(0);
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static List<char> GetOperations()
+        {
+            throw new NotImplementedException(); //TODO
+        }
+
+        private static string RemoveGarbageSymbols(string str)
+        {            
+            return str; //TODO;
+        }
+
+        public static int CalcOperation(int num1, int num2, char operation)
+        {
+            return num1 + num2; //TODO
+        }
+    }
+}
--- LabsTasks/Program.cs
+++ LabsTasks/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using LabsTasks;
 using LabsTasks.Lab2;
 using LabsTasks.ListLearn;
 
-Lab2_7.Solution();
+Lab2_9.Calculate();
