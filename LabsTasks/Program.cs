629b1b43882cb939ce32528cc3623ed92d4410d6
yarosfaros <yaroslav.nizhankovskyi@gmail.com>
2023-03-23 17:50:54 +0200

    Lab3

--- LabsTasks/Program.cs
+++ LabsTasks/Program.cs
@@ -1,6 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using LabsTasks.Lab2;
-using LabsTasks.ListLearn;
 
-Lab2_9.Calculate();
+
+using LabsTasks.Lab3;
+
+var path = "C:\\Users\\aim\\source\\repos\\LabsTasks\\LabsTasks\\test.txt";
+
+using (StreamWriter writer = File.CreateText(path))
+{
+    await writer.WriteLineAsync("First line of example");
+    await writer.WriteLineAsync("and second line");
+}
+
+
+var work = new WorkInCompany();
+
+IndividWork.DoWorkWOthStudent();
